15d3f984b41af64d83bad854fdbef31c0f215e5a
aarnapuvvala <155405226+aarnapuvvala@users.noreply.github.com>
2024-06-21 10:32:51 -0400

    Initial Commit

---
+++ AboutMeApp/AboutMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeAppApp.swift
+//  AboutMeApp
+//
+//  Created by Aarna Puvvala on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
